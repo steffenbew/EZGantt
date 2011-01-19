c4019ef4fd4c8ec915dd81d4eb4e4f70519d841b
domtra <dominik.traenklein@gmail.com>
2011-01-19 20:48:00 +0100

    added getter and setter functions

--- ezgantt.class.php
+++ ezgantt.class.php
@@ -58,16 +58,47 @@ class EZGantt {
 
 	}
 	
+	public function getTitle(){
+	  return $this->title;
+	}
+	
+	public function setTitle($title){
+	  $this->title = $title;
+	}
+	
+	public function getStartDate(){
+	  return $this->start_date;
+	}
+	
+	public function setStartDate($start_date){
+	  $this->start_date = $start_date;
+	}
+	
+	public function getEndDate(){
+	  return $this->end_date;
+	}
+	
+	public function setEndDate($end_date){
+	  $this->end_date = $end_date;
+	}
+	
+	public function getWidth(){
+	  return $this->$width;
+	}
+	
+	public function setWidth($width){
+	  $this->width = $width;
+	}
 	
 	private function _convert_date($date)
 	{
 		return strtotime($date);
 	}
 	
-	
 	public function render()
 	{
 		return $this->events;
+		//$html = '<div id="ezgantt_' . '">'
 	}
 	
 }
